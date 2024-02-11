b0034cc29ecfa71005078808efe757ff706165d4
mdshimul101 <shimulfjr101@gmail.com>
2024-02-11 22:39:38 +0600

    fatch api and show user info in UserCard component

--- src/Components/UserCard.js
+++ src/Components/UserCard.js
@@ -1,30 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const UserCard = () => {
+const UserCard = ({ item }) => {
+  const { image, firstName, lastName, email, address, company } = item;
   return (
     <div className="border hover:border-gray-500 duration-200 rounded-lg p-4">
       <img
-        src="https://robohash.org/Terry.png?set=set4"
+        src={image}
         alt="Avatar"
         className="w-16 h-16 rounded-full mx-auto mb-2"
       />
-      <Link>
+      <Link to={`/item/${item.id}`}>
         <h3 className="text-[#212529] text-center text-2xl font-medium my-5 hover:text-blue-700">
-          Terry Medhurst
+          {firstName} <span> </span>
+          {lastName}
         </h3>
       </Link>
       <p className="text-gray-600 mb-3">
-        <span className="text-gray-700 font-semibold">Email : </span>{" "}
-        atuny0@sohu.com
+        <span className="text-gray-700 font-semibold">Email : </span> {email}
       </p>
       <p className="text-gray-600 mb-3">
-        <span className="text-gray-700 font-semibold">Address : </span> 1745 T
-        Street Southeast, Washington
+        <span className="text-gray-700 font-semibold">Address : </span>{" "}
+        {address.address}, {address.city}
       </p>
       <p className="text-gray-600 mb-3">
         <span className="text-gray-700 font-semibold"> Company Name : </span>
-        Blanda-O'Keefe
+        {company.name}
       </p>
     </div>
   );
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import FilterUser from "../Components/FilterUser";
 import UserCard from "../Components/UserCard";
 
@@ -6,7 +6,36 @@ const Home = () => {
   const [searchData, setSearchData] = useState("");
   const [sortData, setSortData] = useState("");
 
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+
   console.log("Home data", searchData, sortData);
+
+  useEffect(() => {
+    fetch("https://dummyjson.com/users")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result.users);
+          console.log(result.users);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      );
+  }, []);
+
+  if (!isLoaded) {
+    return (
+      <div className="text-center mt-20 text-sky-600">
+        <span className="loading loading-dots loading-lg inline-block"></span>
+      </div>
+    );
+  }
+
   return (
     <div className="space-y-4 my-20">
       <FilterUser
@@ -15,7 +44,11 @@ const Home = () => {
         sortData={sortData}
         setSortData={setSortData}
       ></FilterUser>
-      <UserCard></UserCard>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 my-24 justify-center">
+        {items.map((item) => (
+          <UserCard key={item.id} item={item}></UserCard>
+        ))}
+      </div>
     </div>
   );
 };
